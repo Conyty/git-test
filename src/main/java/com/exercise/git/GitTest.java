6285ae988ceaa0d907aa5eb3b26412dfb0126870
wty <714674423@qq.com>
2022-10-19 18:41:54 +0800

    first commit

---
+++ src/main/java/com/exercise/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.exercise.git;
+
+/**
+ * @author wty
+ * @date 2022/10/19 18:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
