0ab08ccac6aa3e0d848e02f12e455aa3b61c26fe
Conyty <114509762+Conyty@users.noreply.github.com>
2022-10-19 19:33:43 +0800

    pull test

--- src/main/java/com/exercise/git/GitTest.java
+++ src/main/java/com/exercise/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello hotfix test !");
         System.out.println("hello hotfix test !");
         System.out.println("push test !");
+        System.out.println("pull test !");
     }
 }
