e1081e66556a5698ce5fc9551db553ae609d0192
wty <714674423@qq.com>
2022-10-19 19:04:06 +0800

    master fifth commit

--- src/main/java/com/exercise/git/GitTest.java
+++ src/main/java/com/exercise/git/GitTest.java
@@ -6,6 +6,7 @@ package com.exercise.git;
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello master test!");
         System.out.println("hello git!");
         System.out.println("hello git 2 !");
         System.out.println("hello git 3 !");
